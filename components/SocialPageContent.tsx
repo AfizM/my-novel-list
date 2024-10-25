cfeb956578d12ae529a8543ace53042b90ea2f99
AfizM <afiz.mahmud@gmail.com>
2024-10-25 14:21:43 +1100

    Update container width of social page content

--- components/SocialPageContent.tsx
+++ components/SocialPageContent.tsx
@@ -113,7 +113,7 @@ export default function SocialPageContent({ user }: SocialPageContentProps) {
   );
 
   return (
-    <div className="container mx-auto py-8">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-8">
       <Tabs defaultValue="followers">
         <TabsList>
           <TabsTrigger value="followers">
